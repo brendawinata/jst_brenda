8c60b6432cc351bdfe5b7ba2c761cde0c8d1ca4a
brendawinata <84926280+brendawinata@users.noreply.github.com>
2021-05-28 21:30:08 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1882458924:AAG-dx6EjJXTkIzyqGpbr7ItPR6U8zKA_NE'
 const bot = new TelegramBot(token, {polling: true});
 
 
